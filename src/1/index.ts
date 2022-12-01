21167b2ad81030f42157fbf27bc6d5b0a1ba8112
Julien Zapata Duque <jzapata@live.nl>
2022-12-01 19:22:36 +0100

    feat: day 1 (part 1 and 2)

---
+++ src/1/index.ts
@@ -0,0 +1,36 @@
+const decoder = new TextDecoder('utf-8');
+const input = decoder.decode(await Deno.readFile('./input.txt'));
+
+const elves = [];
+
+const items = input.split('\n');
+
+let elfsTotalCalories = 0;
+
+for (let i = 0; i < items.length; i++) {
+  const current = items[i];
+  const next = items[i + 1];
+
+  if (current === '' || next === undefined) {
+    elves.push(elfsTotalCalories);
+    elfsTotalCalories = 0;
+  }
+
+  elfsTotalCalories += +current;
+}
+
+const topThree = elves.toSorted((a, b) => b - a).slice(0, 3);
+
+const highestCalories = elves.reduce(
+  (accum, current) => (current > accum ? current : accum),
+  Number.MIN_SAFE_INTEGER
+);
+
+console.log({ highestCalories, elves });
+
+const topThreeCaloryTotal = topThree.reduce(
+  (total, current) => total + current,
+  0
+);
+
+console.log({ topThree, topThreeCaloryTotal });
