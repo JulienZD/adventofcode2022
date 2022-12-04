e180a62bc0596e23e0f517342bbce0ecc95150b4
Julien <jzapata@live.nl>
2022-12-04 10:40:24 +0100

    feat: day 4 part 2

--- src/4/index.ts
+++ src/4/index.ts
@@ -7,13 +7,16 @@ const isFullyContained = (rangeOne: [number, number], rangeTwo: [number, number]
   return minOne >= minTwo && maxOne <= maxTwo;
 };
 
-const overlappingAssignments = (assignments: string[]) => {
+const overlappingAssignments = (
+  assignments: string[],
+  comparator: (rangeOne: [number, number], rangeTwo: [number, number]) => boolean
+) => {
   let overlaps = 0;
 
   const pairs = assignments.map((a) => a.split(',').map((pair) => pair.split('-').map(Number) as [number, number]));
 
   for (const [rangeOne, rangeTwo] of pairs) {
-    if (isFullyContained(rangeOne, rangeTwo) || isFullyContained(rangeTwo, rangeOne)) {
+    if (comparator(rangeOne, rangeTwo) || comparator(rangeTwo, rangeOne)) {
       overlaps++;
     }
   }
@@ -21,10 +24,20 @@ const overlappingAssignments = (assignments: string[]) => {
   return overlaps;
 };
 
+const hasSingleOverlap = (rangeOne: [number, number], rangeTwo: [number, number]) => {
+  const [minOne, maxOne] = rangeOne;
+  const [minTwo, maxTwo] = rangeTwo;
+
+  return (minOne >= minTwo && minOne <= maxTwo) || (maxOne >= minTwo && maxOne <= maxTwo);
+};
+
 export const partOne: Solver = (input: string) => {
-  return overlappingAssignments(input.split('\n'));
+  return overlappingAssignments(input.split('\n'), isFullyContained);
 };
 
 export const partTwo: Solver = (input: string) => {
-  return null;
+  return overlappingAssignments(
+    input.split('\n'),
+    (rangeOne, rangeTwo) => isFullyContained(rangeOne, rangeTwo) || hasSingleOverlap(rangeOne, rangeTwo)
+  );
 };
