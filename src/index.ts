89b51223e1a5d91eea53c9f0b56a74d67aabc90f
Julien <jzapata@live.nl>
2022-12-04 10:36:31 +0100

    feat: specify benchmark iterations, log solution

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ if (day < 1 || day > 25) {
   Deno.exit(1);
 }
 
+const iterations = Number(Deno.args[1]) || 200;
+
 const solutionDir = `./${day}`;
 
 const input = await readFile(path.join(solutionDir, 'input.txt'));
@@ -18,15 +20,16 @@ const { partOne, partTwo }: { partOne: Solver; partTwo: Solver } = await import(
 
 const benchmark = (fn: () => unknown, solutionNo: number) => {
   const benchmarksInMs: number[] = [];
-  for (let i = 0; i < 200; i++) {
+  let result;
+  for (let i = 0; i < iterations; i++) {
     const start = Date.now();
-    fn();
+    result = fn();
     const end = Date.now();
     benchmarksInMs.push(end - start);
   }
   const averageTimeMs = benchmarksInMs.reduce((total, current) => total + current, 0) / benchmarksInMs.length;
 
-  console.log(`Solution ${solutionNo}:`, `took an average of ${averageTimeMs}ms`);
+  console.log(`Solution ${solutionNo}: ${result}`, `took an average of ${averageTimeMs}ms (${iterations} iterations)`);
 };
 
 benchmark(() => partOne(input), 1);
