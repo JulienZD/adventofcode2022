2c58f517175fff7199bd5bd4a2ad40449dce7640
Julien <jzapata@live.nl>
2022-12-04 10:46:52 +0100

    refactor: day 4 immutable style

--- src/4/index.ts
+++ src/4/index.ts
@@ -7,28 +7,23 @@ const isFullyContained = (rangeOne: [number, number], rangeTwo: [number, number]
   return minOne >= minTwo && maxOne <= maxTwo;
 };
 
-const overlappingAssignments = (
-  assignments: string[],
-  comparator: (rangeOne: [number, number], rangeTwo: [number, number]) => boolean
-) => {
-  let overlaps = 0;
-
-  const pairs = assignments.map((a) => a.split(',').map((pair) => pair.split('-').map(Number) as [number, number]));
-
-  for (const [rangeOne, rangeTwo] of pairs) {
-    if (comparator(rangeOne, rangeTwo) || comparator(rangeTwo, rangeOne)) {
-      overlaps++;
-    }
-  }
-
-  return overlaps;
-};
-
+// Used in part 2
 const hasSingleOverlap = (rangeOne: [number, number], rangeTwo: [number, number]) => {
   const [minOne, maxOne] = rangeOne;
   const [minTwo, maxTwo] = rangeTwo;
 
   return (minOne >= minTwo && minOne <= maxTwo) || (maxOne >= minTwo && maxOne <= maxTwo);
+};
+
+const overlappingAssignments = (
+  assignments: string[],
+  comparator: (rangeOne: [number, number], rangeTwo: [number, number]) => boolean
+) => {
+  const overlappingAssignments = assignments
+    .map((a) => a.split(',').map((pair) => pair.split('-').map(Number) as [number, number]))
+    .filter(([rangeOne, rangeTwo]) => comparator(rangeOne, rangeTwo) || comparator(rangeTwo, rangeOne));
+
+  return overlappingAssignments.length;
 };
 
 export const partOne: Solver = (input: string) => {
