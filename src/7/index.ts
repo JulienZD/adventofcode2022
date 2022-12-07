512b3b83bafa3c1c05e3b60a65b195af734c4807
Julien <jzapata@live.nl>
2022-12-07 21:21:42 +0100

    feat: day 7 part 2

--- src/7/index.ts
+++ src/7/index.ts
@@ -1,11 +1,16 @@
 import type { Solver } from '../aoc.d.ts';
 
+const MIN_AVAILABLE_SPACE = 30_000_000;
+const MAX_AVAILABLE_SPACE = 70_000_000;
+
 interface Directory {
   name: string;
   files: number[];
   directories: Directory[];
   parent?: Directory;
 }
+
+type DirectoryWithSize = Directory & { size: number; directories: DirectoryWithSize[] };
 
 const determineLargestSizes = (terminalOutput: string[]) => {
   const root: Directory = {
@@ -51,30 +56,59 @@ const determineLargestSizes = (terminalOutput: string[]) => {
     currentDirectory.files.push(+size);
   }
 
-  const determineDirSize = (directory: Directory, total = 0): number => {
-    const totalFilesSize = directory.files.reduce((fileTotal, fileSize) => fileTotal + fileSize, 0);
-    const totalSizeOfSubDirectories = directory.directories.reduce(
-      (dirTotal, dir) => dirTotal + determineDirSize(dir),
-      0
-    );
-
-    return total + totalFilesSize + totalSizeOfSubDirectories;
-  };
-
   let totalSize = 0;
 
-  const calculateDirectorySize = (directory: Directory): void => {
+  const calculateDirectorySize = (directory: Directory): DirectoryWithSize => {
     const dirSize = determineDirSize(directory);
     if (dirSize < 100_000) {
       totalSize += dirSize;
     }
 
-    directory.directories.forEach(calculateDirectorySize);
+    return {
+      ...directory,
+      size: dirSize,
+      directories: directory.directories.map(calculateDirectorySize),
+    };
   };
 
-  calculateDirectorySize(root);
+  const withSizes = calculateDirectorySize(root);
 
-  return totalSize;
+  const availableSpace = MAX_AVAILABLE_SPACE - withSizes.size;
+  const spaceToDelete = MIN_AVAILABLE_SPACE - availableSpace;
+
+  const result = findSmallestDirectory(withSizes, spaceToDelete);
+
+  return result.size;
+};
+
+const findSmallestDirectory = (directory: DirectoryWithSize, minSize: number): DirectoryWithSize => {
+  const isElegible = directory.size >= minSize;
+
+  if (!isElegible) {
+    return directory;
+  }
+
+  const eligibleSubdirectories: DirectoryWithSize[] = [];
+  for (const subDir of directory.directories) {
+    const smallestEligible = findSmallestDirectory(subDir, minSize);
+    eligibleSubdirectories.push(smallestEligible);
+  }
+
+  const sortedDirectories = [directory, ...eligibleSubdirectories]
+    .toSorted((a, b) => a.size - b.size)
+    .filter(({ size }) => size >= minSize);
+
+  return sortedDirectories[0];
+};
+
+const determineDirSize = (directory: Directory, total = 0): number => {
+  const totalFilesSize = directory.files.reduce((fileTotal, fileSize) => fileTotal + fileSize, 0);
+  const totalSizeOfSubDirectories = directory.directories.reduce(
+    (dirTotal, dir) => dirTotal + determineDirSize(dir),
+    0
+  );
+
+  return total + totalFilesSize + totalSizeOfSubDirectories;
 };
 
 export const partOne: Solver = (input: string) => {
