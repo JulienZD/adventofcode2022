42c3da1dc2f0c3f651619bcce86c80f3e4e67042
Julien Zapata Duque <jzapata@live.nl>
2022-12-11 15:58:56 +0100

    feat: day 10 (part 2)

--- src/10/index.ts
+++ src/10/index.ts
@@ -32,10 +32,50 @@ const executeInstructionsPartOne = (instructions: string[]) => {
   return totalSignalStrength;
 };
 
+const executeInstructionsPartTwo = (instructions: string[]) => {
+  let register = 1;
+  let cycles = 0;
+  let position = 0;
+  let output = '\n';
+
+  const tick = () => {
+    cycles++;
+
+    const shouldDrawLitPixel = position === register || position + 1 === register || position - 1 === register;
+
+    output += shouldDrawLitPixel ? '#' : '.';
+    position++;
+
+    const reachedEndOfScreen = cycles % 40 === 0;
+
+    if (reachedEndOfScreen) {
+      output += '\n';
+      position = 0;
+    }
+  };
+
+  instructions.forEach((instruction) => {
+    if (instruction === 'noop') {
+      tick();
+      return;
+    }
+
+    const [, value] = instruction.split(' ');
+    if (instruction.startsWith('addx')) {
+      tick();
+      tick();
+
+      register += +value;
+    }
+  });
+
+  return output;
+};
+
 export const partOne: Solver = (input: string) => {
   return executeInstructionsPartOne(input.split('\n'));
 };
 
 export const partTwo: Solver = (input: string) => {
-  return null;
+  return executeInstructionsPartTwo(input.split('\n'));
 };
